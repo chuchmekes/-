51d9be9f79e9dde5c05190c68ea3e58e4a4ed2c1
chuchmekes <91018093+chuchmekes@users.noreply.github.com>
2021-10-31 15:38:53 +0300

    Update 75.py

--- 75.py
+++ 75.py
@@ -1,6 +1,7 @@
 num=[123, 234, 345, 456]
 for i in num:
     print(i)
-num2 = int(input("Введите трехзначное число: "))
-num.index(int(num2), [0, 3])
-
+try:
+    print(num.index(input("Введите число: ")))
+except ValueError:
+    print("This is not in the list")
